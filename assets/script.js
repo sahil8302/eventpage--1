8b8f8a798fc01ea1617a2a99f96a9acc5d682482
sahil <sahil@sahils-MacBook-Air.local>
2025-09-15 14:24:13 +0530

    add some js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,7 @@
 let backtotop = document.querySelector(".arrow-btn");
+let navbar = document.querySelector(".navbar");
+let navcolored = document.querySelector(".nav-colored");
+let navtransparent = document.querySelector(".nav-transparent");
 
 window.onscroll = function() {
     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
@@ -13,4 +16,14 @@ backtotop.addEventListener("click", function() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 
+});
+
+window.addEventListener("scroll", function() {
+    if (window.scrollY > 300) {
+        navbar.classList.add("nav-colored");
+        navbar.classList.remove("nav-transparent");
+    } else {
+        navbar.classList.add("nav-transparent");
+        navbar.classList.remove("nav-colored");
+    }
 });
